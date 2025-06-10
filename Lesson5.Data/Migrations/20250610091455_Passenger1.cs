01ae65fca1305be5bcda36b4007ae6bc59870f16
tzippykovalsky <9098537@gmail.com>
2025-06-10 12:17:38 +0300

    add migration

---
+++ Lesson5.Data/Migrations/20250610091455_Passenger1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lesson5.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Passenger1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Passes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PassportNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlightPassenger",
+                columns: table => new
+                {
+                    FlightsId = table.Column<int>(type: "int", nullable: false),
+                    PassengersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightPassenger", x => new { x.FlightsId, x.PassengersId });
+                    table.ForeignKey(
+                        name: "FK_FlightPassenger_Flights_FlightsId",
+                        column: x => x.FlightsId,
+                        principalTable: "Flights",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FlightPassenger_Passes_PassengersId",
+                        column: x => x.PassengersId,
+                        principalTable: "Passes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlightPassenger_PassengersId",
+                table: "FlightPassenger",
+                column: "PassengersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlightPassenger");
+
+            migrationBuilder.DropTable(
+                name: "Passes");
+        }
+    }
+}
